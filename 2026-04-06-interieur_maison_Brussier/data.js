60fb7b1381b53606dcb7670cc34a7cf7864a2d6e
Baldhead94 <aubertsylvain@free.fr>
2026-04-06 12:10:21 +0200

    maj maison brssier
    intérieur

---
+++ 2026-04-06-interieur_maison_Brussier/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2026-04-06-grignan---table-5-rue-sous-les-remparts",
+      "name": "2026-04-06-GRIGNAN - table 5 rue sous les remparts",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.4175837557344586,
+        "pitch": 0.04699329834468813,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
