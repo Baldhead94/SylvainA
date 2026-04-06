7264d9be5d49f17857343e016a0520dad410c27c
Baldhead94 <aubertsylvain@free.fr>
2026-04-06 18:32:30 +0200

    ajout rochecourbière
    ajout grotte de rochecourbière

---
+++ 2026-04-06-Grignan_La_Grotte_de_Rochecourbiere/data.js
@@ -0,0 +1,390 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20260406_143417_00_240",
+      "name": "IMG_20260406_143417_00_240",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.519493911310148,
+        "pitch": -0.07240596406875532,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.835394043810922,
+          "pitch": 0.1914879573912991,
+          "rotation": 5.497787143782138,
+          "target": "1-img_20260406_143528_00_241"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8012237960663171,
+          "pitch": -0.3167674754076799,
+          "title": "Grignan - La Grotte de Rochecourbière",
+          "text": "où Madame de Sévigné aimait à se promener"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20260406_143528_00_241",
+      "name": "IMG_20260406_143528_00_241",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.209855245249342,
+        "pitch": 0.09398784133836102,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8928450845271705,
+          "pitch": 0.5876707726333414,
+          "rotation": 7.0685834705770345,
+          "target": "0-img_20260406_143417_00_240"
+        },
+        {
+          "yaw": 1.6914093598926456,
+          "pitch": 0.3911096056678254,
+          "rotation": 5.497787143782138,
+          "target": "2-img_20260406_143759_00_243"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_20260406_143759_00_243",
+      "name": "IMG_20260406_143759_00_243",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.7000695331988247,
+        "pitch": -0.011626048452638926,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22807195154379656,
+          "pitch": 0.3854690799430003,
+          "rotation": 0.7853981633974483,
+          "target": "1-img_20260406_143528_00_241"
+        },
+        {
+          "yaw": -0.5150573035621573,
+          "pitch": 0.45314682413722984,
+          "rotation": 0,
+          "target": "0-img_20260406_143417_00_240"
+        },
+        {
+          "yaw": -2.8801271592896143,
+          "pitch": 0.5491622311508664,
+          "rotation": 0,
+          "target": "3-img_20260406_143854_00_244"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-img_20260406_143854_00_244",
+      "name": "IMG_20260406_143854_00_244",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.6875818392147295,
+        "pitch": 0.049881207438662756,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5576279373676449,
+          "pitch": 0.5792991183984562,
+          "rotation": 0,
+          "target": "2-img_20260406_143759_00_243"
+        },
+        {
+          "yaw": -0.4610017437002405,
+          "pitch": 0.22796969862965,
+          "rotation": 0,
+          "target": "4-img_20260406_144006_00_245"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-img_20260406_144006_00_245",
+      "name": "IMG_20260406_144006_00_245",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.833281861595596,
+        "pitch": 0.10541851100274968,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.758662948417607,
+          "pitch": 0.4997199842691149,
+          "rotation": 0,
+          "target": "5-img_20260406_144244_00_246"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-img_20260406_144244_00_246",
+      "name": "IMG_20260406_144244_00_246",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.4188790204786468,
+        "pitch": 0.018807492074163434,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5870823966290075,
+          "pitch": 0.321988674883368,
+          "rotation": 0,
+          "target": "6-img_20260406_144424_00_247"
+        },
+        {
+          "yaw": -2.597270096310485,
+          "pitch": 0.4036032754600676,
+          "rotation": 0,
+          "target": "4-img_20260406_144006_00_245"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-img_20260406_144424_00_247",
+      "name": "IMG_20260406_144424_00_247",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.34907363229992683,
+        "pitch": -0.009398784133836102,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.558727893347351,
+          "pitch": 0.18592487392217727,
+          "rotation": 0,
+          "target": "7-img_20260406_144533_00_248"
+        },
+        {
+          "yaw": -2.6104506717592137,
+          "pitch": 0.39065102085728043,
+          "rotation": 0,
+          "target": "5-img_20260406_144244_00_246"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-img_20260406_144533_00_248",
+      "name": "IMG_20260406_144533_00_248",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.772419516349089,
+        "pitch": 0.06579148893685272,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.076126145827491,
+          "pitch": 0.4508274687816254,
+          "rotation": 0,
+          "target": "6-img_20260406_144424_00_247"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "2026-04-06-Grignan - La Grotte de Rochecourbière",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
