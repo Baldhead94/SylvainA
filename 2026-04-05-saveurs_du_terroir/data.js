5cbd606ed584ceafdce5a2f1c0604efd66645fbe
Baldhead94 <aubertsylvain@free.fr>
2026-04-06 10:26:47 +0200

    site web dedié a la visite 3d
    site web dédié a la visite 3d

---
+++ 2026-04-05-saveurs_du_terroir/data.js
@@ -0,0 +1,264 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2026-04-05-grigan-saveurs_du_terroir-exterieur",
+      "name": "2026-04-05-GRIGAN-Saveurs_du_terroir exterieur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.5156105838072325,
+        "pitch": -0.25421982729201353,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5703284421963257,
+          "pitch": 0.2123697356401344,
+          "rotation": 0,
+          "target": "1-2026-04-05-grigan-saveurs_du_terroir-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5987991680301903,
+          "pitch": -0.07967679921141801,
+          "title": "Epicerie Fine \"Saveurs du Terroir\"",
+          "text": "Bio Locale Sympa !"
+        }
+      ]
+    },
+    {
+      "id": "1-2026-04-05-grigan-saveurs_du_terroir-4",
+      "name": "2026-04-05-GRIGAN-Saveurs_du_terroir (4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.936579022140185,
+        "pitch": 0.09962711181866268,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.039783207298898,
+          "pitch": 0.5628239403860782,
+          "rotation": 5.497787143782138,
+          "target": "2-2026-04-05-grigan-saveurs_du_terroir-3"
+        },
+        {
+          "yaw": -1.7387399914218715,
+          "pitch": 0.6596293317503683,
+          "rotation": 0.7853981633974483,
+          "target": "4-2026-04-05-grigan-saveurs_du_terroir-1"
+        },
+        {
+          "yaw": 0.12911237418435917,
+          "pitch": 0.6781675522219732,
+          "rotation": 0,
+          "target": "0-2026-04-05-grigan-saveurs_du_terroir-exterieur"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2026-04-05-grigan-saveurs_du_terroir-3",
+      "name": "2026-04-05-GRIGAN-Saveurs_du_terroir (3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.3613562722849153,
+        "pitch": 0.09398784133836102,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.086437292022941,
+          "pitch": 0.5727450685856539,
+          "rotation": 7.0685834705770345,
+          "target": "1-2026-04-05-grigan-saveurs_du_terroir-4"
+        },
+        {
+          "yaw": -1.3860944820793808,
+          "pitch": 0.8026977681151308,
+          "rotation": 1.5707963267948966,
+          "target": "3-2026-04-05-grigan-saveurs_du_terroir-5"
+        },
+        {
+          "yaw": 1.5894981259290883,
+          "pitch": 0.18886066443685934,
+          "rotation": 5.497787143782138,
+          "target": "4-2026-04-05-grigan-saveurs_du_terroir-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2026-04-05-grigan-saveurs_du_terroir-5",
+      "name": "2026-04-05-GRIGAN-Saveurs_du_terroir (5)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.4108182731715484,
+        "pitch": 0.1646111410190052,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3758886959424554,
+          "pitch": 0.6978451892249744,
+          "rotation": 0,
+          "target": "4-2026-04-05-grigan-saveurs_du_terroir-1"
+        },
+        {
+          "yaw": 2.9898387144244296,
+          "pitch": 0.6951325094535878,
+          "rotation": 0,
+          "target": "2-2026-04-05-grigan-saveurs_du_terroir-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-2026-04-05-grigan-saveurs_du_terroir-1",
+      "name": "2026-04-05-GRIGAN-Saveurs_du_terroir (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.123890644182179,
+        "pitch": 0.25745150001366746,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.27732824975221,
+          "pitch": 0.6916250221894558,
+          "rotation": 0,
+          "target": "3-2026-04-05-grigan-saveurs_du_terroir-5"
+        },
+        {
+          "yaw": 1.033670667095203,
+          "pitch": 0.6938996287828552,
+          "rotation": 5.497787143782138,
+          "target": "1-2026-04-05-grigan-saveurs_du_terroir-4"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Saveurs du Terroir - Grignan",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
