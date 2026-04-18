2780ae4897c387b48e05c8115ebf414d1a598679
Baldhead94 <aubertsylvain@free.fr>
2026-04-18 18:47:57 +0200

    rajout vacherot et lecoufle
    rajout vacherot et lecoufle

---
+++ 2026-04-18-vacherot&lecoufle/data.js
@@ -0,0 +1,714 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lorchideefr_14",
+      "name": "lorchidee.fr_(14)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.0008955357766975425,
+        "pitch": 0.013250685505157733,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.004030424570762747,
+          "pitch": 0.6322014490337757,
+          "rotation": 0,
+          "target": "1-lorchideefr_01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4326731573245066,
+          "pitch": 0.5772093800012197,
+          "title": "Vacherot &amp; Lecoufle",
+          "text": "Depuis 1886 - Créateurs, Sélectionneurs d'Orchidées"
+        }
+      ]
+    },
+    {
+      "id": "1-lorchideefr_01",
+      "name": "lorchidee.fr_(01)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.9856635901180901,
+        "pitch": 0.1651917263859417,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.018510692060481,
+          "pitch": 1.1425794091249912,
+          "rotation": 3.141592653589793,
+          "target": "0-lorchideefr_14"
+        },
+        {
+          "yaw": 0.6839421761203237,
+          "pitch": 1.0710378060275634,
+          "rotation": 4.71238898038469,
+          "target": "2-lorchideefr_2"
+        },
+        {
+          "yaw": 1.2928696917137898,
+          "pitch": 1.0684926601267488,
+          "rotation": 7.853981633974483,
+          "target": "13-lorchideefr_13"
+        },
+        {
+          "yaw": -0.5575979507555822,
+          "pitch": 1.0297016325676278,
+          "rotation": 0,
+          "target": "2-lorchideefr_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-lorchideefr_2",
+      "name": "lorchidee.fr_(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.176465026708481,
+        "pitch": 0.0905797247547202,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7701118429499836,
+          "pitch": 1.0315169397426391,
+          "rotation": 1.5707963267948966,
+          "target": "1-lorchideefr_01"
+        },
+        {
+          "yaw": -1.9444014411200232,
+          "pitch": 0.9519510628455023,
+          "rotation": 0,
+          "target": "3-lorchideefr_3"
+        },
+        {
+          "yaw": -0.037969091744814776,
+          "pitch": 0.8495443296303016,
+          "rotation": 0,
+          "target": "1-lorchideefr_01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lorchideefr_3",
+      "name": "lorchidee.fr_(3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.386780689481105,
+        "pitch": 0.2888163656244327,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.395564163006636,
+          "pitch": 1.2443554006912176,
+          "rotation": 3.141592653589793,
+          "target": "2-lorchideefr_2"
+        },
+        {
+          "yaw": 2.3936299827150513,
+          "pitch": 1.001134725874989,
+          "rotation": 1.5707963267948966,
+          "target": "8-lorchideefr_8"
+        },
+        {
+          "yaw": -2.4750618988301056,
+          "pitch": 0.7635973389688466,
+          "rotation": 0,
+          "target": "8-lorchideefr_8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-lorchideefr_4",
+      "name": "lorchidee.fr_(4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.368525337547375,
+        "pitch": 0.08709694430514858,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4019428606436843,
+          "pitch": 1.0325137251433585,
+          "rotation": 3.141592653589793,
+          "target": "5-lorchideefr_5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-lorchideefr_5",
+      "name": "lorchidee.fr_(5)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.8093729318072107,
+        "pitch": 0.1592920218721563,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7840299715571071,
+          "pitch": 1.0997767268359588,
+          "rotation": 3.141592653589793,
+          "target": "6-lorchideefr_6"
+        },
+        {
+          "yaw": 0.7732540900875549,
+          "pitch": 0.9997348756676487,
+          "rotation": 0,
+          "target": "4-lorchideefr_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-lorchideefr_6",
+      "name": "lorchidee.fr_(6)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.5055746884958214,
+        "pitch": 0.042035394660707937,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5414814329825006,
+          "pitch": 0.897396778486609,
+          "rotation": 0,
+          "target": "5-lorchideefr_5"
+        },
+        {
+          "yaw": 0.5418640041847596,
+          "pitch": 0.9890626981180759,
+          "rotation": 3.141592653589793,
+          "target": "7-lorchideefr_7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-lorchideefr_7",
+      "name": "lorchidee.fr_(7)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.3740794284424478,
+        "pitch": 0.008849556770673672,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.37902168610661136,
+          "pitch": 0.9627651106677497,
+          "rotation": 3.141592653589793,
+          "target": "8-lorchideefr_8"
+        },
+        {
+          "yaw": 0.3691509474729475,
+          "pitch": 0.8645189626593393,
+          "rotation": 0,
+          "target": "6-lorchideefr_6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-lorchideefr_8",
+      "name": "lorchidee.fr_(8)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.2419581414103114,
+        "pitch": 0.32300882212966364,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8734086229567772,
+          "pitch": 1.1779934514808517,
+          "rotation": 1.5707963267948966,
+          "target": "10-lorchideefr_10"
+        },
+        {
+          "yaw": 0.5025189598338713,
+          "pitch": 0.7932568017245636,
+          "rotation": 0,
+          "target": "7-lorchideefr_7"
+        },
+        {
+          "yaw": 0.35818668845075763,
+          "pitch": 1.070853185435304,
+          "rotation": 4.71238898038469,
+          "target": "3-lorchideefr_3"
+        },
+        {
+          "yaw": -0.9868832874372124,
+          "pitch": 0.6136346545882176,
+          "rotation": 0,
+          "target": "3-lorchideefr_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-lorchideefr_9",
+      "name": "lorchidee.fr_(9)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5600978698394279,
+        "pitch": 0.24483773732204028,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.704519611154705,
+          "pitch": 1.1307350795854987,
+          "rotation": 4.71238898038469,
+          "target": "10-lorchideefr_10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-lorchideefr_10",
+      "name": "lorchidee.fr_(10)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.5463834077465943,
+        "pitch": 0.3340707680930155,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3511007270662425,
+          "pitch": 1.3205440575926808,
+          "rotation": 3.141592653589793,
+          "target": "11-lorchideefr_11"
+        },
+        {
+          "yaw": 0.7514918176238634,
+          "pitch": 1.245618921872122,
+          "rotation": 1.5707963267948966,
+          "target": "9-lorchideefr_9"
+        },
+        {
+          "yaw": 0.04078457708752481,
+          "pitch": 1.2159817530382178,
+          "rotation": 4.71238898038469,
+          "target": "8-lorchideefr_8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-lorchideefr_11",
+      "name": "lorchidee.fr_(11)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.7246652939297711,
+        "pitch": 0.11799409027567265,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.748337975406141,
+          "pitch": 1.0798696064756221,
+          "rotation": 3.141592653589793,
+          "target": "12-lorchideefr_12"
+        },
+        {
+          "yaw": 0.736064632525169,
+          "pitch": 0.9798879588081597,
+          "rotation": 0,
+          "target": "10-lorchideefr_10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-lorchideefr_12",
+      "name": "lorchidee.fr_(12)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.08615174064055786,
+        "pitch": 0.04646017304605188,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.038741851825502494,
+          "pitch": 1.0527341986403158,
+          "rotation": 3.141592653589793,
+          "target": "13-lorchideefr_13"
+        },
+        {
+          "yaw": 0.028967732343723185,
+          "pitch": 0.9689681471169145,
+          "rotation": 0,
+          "target": "11-lorchideefr_11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-lorchideefr_13",
+      "name": "lorchidee.fr_(13)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4055523192052011,
+          "pitch": 0.9393590060038335,
+          "rotation": 4.71238898038469,
+          "target": "1-lorchideefr_01"
+        },
+        {
+          "yaw": -0.1117498821677998,
+          "pitch": 0.8810075575560958,
+          "rotation": 0,
+          "target": "12-lorchideefr_12"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Lorchidee.fr Vacherot&Lecoufle depuis 1886",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
